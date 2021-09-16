991f58185316746d28e7930b8f41064d4de9ea19
Omar Ouhra <omarmaroc26@gmail.com>
2021-09-16 15:06:58 +0100

    Carousel Component done ✅

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon clone - @omar.code</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      {/* Header */ }
-      
+
+      {/* Header */}
       <Header />
+
+      {/* Main  */}
+      <main className='max-w-screen-2xl mx-auto'>
+        {/* banner */}
+        <Banner />
+      </main>
     </div>
   );
 }
