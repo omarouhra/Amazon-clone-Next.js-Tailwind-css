5e15968c4900aa0f0116cd98e4ba23477ea5a78c
Omar Ouhra <omarmaroc26@gmail.com>
2021-09-21 12:45:58 +0100

    Next Config Fix ✅

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped. Doesn't make much sense, but how it is
-      fs: false, // the solution
-    };
-
-    return config;
-  },
 };
