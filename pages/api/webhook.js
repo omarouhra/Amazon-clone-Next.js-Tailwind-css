edde971492ef42d0a70857d4cb0eb8dfc2b560be
Omar Ouhra <omarmaroc26@gmail.com>
2021-09-30 13:17:20 +0100

    STRIPE WEBHOOK SETUP

---
+++ pages/api/webhook.js
@@ -0,0 +1,19 @@
+import { buffer } from "micro";
+import * as admin from "firebase-admin";
+
+// Secure a connection to Firebase from the backend
+const serviceAccount = require("../../permissions.json");
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+  : admin.app();
+
+// Establish conneciton to Stripe
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+export default async (req, res) => {
+  if (req.method === "POST") {
+  }
+};
